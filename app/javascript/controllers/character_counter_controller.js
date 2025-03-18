e87a394ee6f14316d1c7e9090680e1af0600c9dd
Jelani Woods <jelani@firstdraft.com>
2025-03-18 16:11:49 -0500

    Change color of count when count exceeds maximum

--- app/javascript/controllers/character_counter_controller.js
+++ app/javascript/controllers/character_counter_controller.js
@@ -3,6 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="character-counter"
 export default class extends Controller {
   static targets = [ "input", "counter" ]
+  static values = {
+    max: { type: Number, default: 200 }
+  }
 
   initialize() {
     this.count = this.countCharacters.bind(this)
@@ -17,6 +20,13 @@ export default class extends Controller {
   }
 
   countCharacters() {
-    this.counterTarget.innerHTML = this.inputTarget.value.length.toString()
+    let number = this.inputTarget.value.length
+    this.counterTarget.innerHTML = number.toString()
+
+    if (number > this.maxValue) {
+      this.counterTarget.classList.add("text-danger")
+    } else if (this.counterTarget.classList.contains("text-danger")){
+      this.counterTarget.classList.remove("text-danger")
+    }
   }
 }
