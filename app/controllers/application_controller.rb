74e37fedee448e5f9aad1fc180cb4dd27ff05934
Jelani Woods <jelani@firstdraft.com>
2025-03-25 11:46:30 -0500

    revert to :modern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: { safari: 17.2, chrome: 116, firefox: 121, opera: 106, ie: false }
+  allow_browser versions: :modern
 
   before_action :authenticate_user!
   before_action :set_user_search, if: -> { current_user.present? }
